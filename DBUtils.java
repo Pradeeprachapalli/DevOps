c442b953b31c5c683e34fcfe65985c8149057949
Pradeeprachapalli <rpradeeprp9@yahoo.com>
2022-12-24 18:47:52 +0530

    Commit

--- DBUtils.java
+++ DBUtils.java
@@ -1,4 +1,5 @@
 Hi this is Pradeep
 First time im understand GIT
 God has to help
-To understand and remember the GIT as it is important.
+To understand and remember the GIT as it is importanti.
+I Opened files and going to close.
